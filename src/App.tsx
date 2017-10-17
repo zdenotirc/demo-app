d2c170d7369fc9e7879b9d948d177ae164926738
Zdeno Tirc <Zdenko.Tirc@infor.com>
2017-10-17 16:11:40 +0200

    Created staless functional component TodoForm.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import './App.css';
 
+import { TodoForm } from './components/todo/TodoForm';
+
 interface AppProps { }
 
 interface AppState {
   todos: TodoItem[];
+
+  currentTodo: string;
 }
 
 interface TodoItem {
@@ -21,21 +25,31 @@ class App extends React.Component<AppProps, AppState> {
         { id: 1, name: 'Learn JSX', isComplete: true },
         { id: 2, name: 'Professional C#', isComplete: false },
         { id: 3, name: 'Bitcoin Fundamentals', isComplete: false },
-      ]
+      ],
+      currentTodo: ''
     };
+
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e: React.FormEvent<HTMLInputElement>) {
+    this.setState({
+      currentTodo: e.currentTarget.value
+    });
   }
 
   render() {
     return (
       <div className="App">
         <div className="Todo-App">
-          <form>
-            <input type="text" />
-          </form>
+          <TodoForm handleInput={this.handleInput} currentTodo={this.state.currentTodo} />
           <div className="Todo-List">
             <ul>
               {this.state.todos.map(todo =>
-                <li key={todo.id}><input type="checkbox" defaultChecked={todo.isComplete} />{todo.name}</li>)}
+                <li key={todo.id}>
+                  <input type="checkbox" defaultChecked={todo.isComplete} />
+                  {todo.name}
+                </li>)}
             </ul>
           </div>
         </div>
---
+++ src/components/todo/TodoForm.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface TodoFormProps {
+    handleInput: (e: React.FormEvent<HTMLInputElement>) => void;
+
+    currentTodo: string;
+}
+
+export const TodoForm = (props: TodoFormProps) => (
+    <div>
+        <form>
+            <input type="text" onChange={props.handleInput} value={props.currentTodo} />
+        </form>
+    </div>
+);
