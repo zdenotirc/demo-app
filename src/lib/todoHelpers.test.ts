968c53ac5c49f4d2bbcd068ac1e6c53c0fb58987
Zdeno Tirc <Zdenko.Tirc@infor.com>
2017-10-19 15:52:17 +0200

    12. Update Data in a List without Mutations

--- src/lib/todoHelpers.test.ts
+++ src/lib/todoHelpers.test.ts
@@ -1,4 +1,4 @@
-import { addTodo } from './todoHelpers';
+import { addTodo, findById, toggleTodo, updateTodo } from './todoHelpers';
 
 test('addTodo should add the passed todo to the list', () => {
     const startTodo = [
@@ -14,9 +14,9 @@ test('addTodo should add the passed todo to the list', () => {
         { id: 3, name: 'Three', isComplete: false }
     ];
 
-    const result = addTodo(startTodo, newTodo);
+    const actual = addTodo(startTodo, newTodo);
 
-    expect(result).toEqual(expected);
+    expect(actual).toEqual(expected);
 });
 
 test('addTodo should not mutate the existing todo array', () => {
@@ -27,7 +27,69 @@ test('addTodo should not mutate the existing todo array', () => {
 
     const newTodo = { id: 3, name: 'Three', isComplete: false };
 
-    const result = addTodo(startTodo, newTodo);
+    const actual = addTodo(startTodo, newTodo);
 
-    expect(result).not.toBe(startTodo);
+    expect(actual).not.toBe(startTodo);
+});
+
+test('findById should return the expected item from an array', () => {
+    const startTodo = [
+        { id: 1, name: 'One', isComplete: false },
+        { id: 2, name: 'Two', isComplete: false },
+        { id: 3, name: 'Three', isComplete: false }
+    ];
+
+    const expected = { id: 2, name: 'Two', isComplete: false };
+    const actual = findById(2, startTodo);
+
+    expect(actual).toEqual(expected);
+});
+
+test('toggleTodo should toggle isComplete prop of a todo', () => {
+    const startTodo = { id: 1, name: 'One', isComplete: false };
+    const expected = { id: 1, name: 'One', isComplete: true };
+
+    const actual = toggleTodo(startTodo);
+    expect(actual).toEqual(expected);
+});
+
+test('toggleTodo should not mutate original todo', () => {
+    const startTodo = { id: 1, name: 'One', isComplete: false };
+
+    const actual = toggleTodo(startTodo);
+    expect(actual).not.toBe(startTodo);
+});
+
+test('updateTodo should update an item by id', () => {
+    const startTodo = [
+        { id: 1, name: 'One', isComplete: false },
+        { id: 2, name: 'Two', isComplete: false },
+        { id: 3, name: 'Three', isComplete: false }
+    ];
+
+    const updatedTodo = { id: 2, name: 'Two', isComplete: true }
+
+    const expectedTodo = [
+        { id: 1, name: 'One', isComplete: false },
+        { id: 2, name: 'Two', isComplete: true },
+        { id: 3, name: 'Three', isComplete: false }
+    ];
+
+    const actual = updateTodo(startTodo, updatedTodo);
+
+    expect(actual).toEqual(expectedTodo);
+});
+
+test('updateTodo should not mutate original array', () => {
+    const startTodo = [
+        { id: 1, name: 'One', isComplete: false },
+        { id: 2, name: 'Two', isComplete: false },
+        { id: 3, name: 'Three', isComplete: false }
+    ];
+
+    const updatedTodo = { id: 2, name: 'Two', isComplete: true }
+
+    const actual = updateTodo(startTodo, updatedTodo);
+
+    expect(actual).not.toBe(startTodo);
 });
--- src/lib/todoHelpers.ts
+++ src/lib/todoHelpers.ts
@@ -2,14 +2,29 @@ import { TodoItemModel } from '../models/TodoItem';
 
 export const addTodo = (list: TodoItemModel[], item: TodoItemModel) => {
     return [...list, item];
-    // return list.concat(item);
 };
 
-/*
-export const addTodo = (list: TodoItemModel[], item: TodoItemModel) => {
-    // mutates existing list
-    list.push(item);
-    return list;
-};*/
-
 export const generateId = () => Math.floor(Math.random() * 100000);
+
+export const findById = (id: number, list: TodoItemModel[]) => {
+    const item = list.find(i => i.id === id);
+
+    return item;
+};
+
+export const toggleTodo = (item: TodoItemModel) => {
+    return {
+        ...item,
+        isComplete: !item.isComplete
+    };
+}
+
+export const updateTodo = (list: TodoItemModel[], item: TodoItemModel) => {
+    return list.map((i) => {
+        if (i.id === item.id) {
+            i = item;
+        }
+
+        return i;
+    });
+}
